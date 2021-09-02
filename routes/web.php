35c26205c9ac14f8cdea4e88678d4e866abb4cdd
gianni <bosciu@icloud.com>
2021-09-02 11:24:32 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,71 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $gateway = new Braintree\Gateway([
+        'environment' => config('services.braintree.environment'),
+        'merchantId' => env('BTREE_MERCHANT_ID'),
+        'publicKey' => env('BTREE_PUBLIC_KEY'),
+        'privateKey' => env('BTREE_PRIVATE_KEY')
+    ]);
+
+    $token = $gateway->ClientToken()->generate();
+
+    return view('welcome', ['token' => $token]);
+});
+
+Route::post('/checkout', function (Request $request) {
+    $gateway = new Braintree\Gateway([
+        'environment' => config('services.braintree.environment'),
+        'merchantId' => env('BTREE_MERCHANT_ID'),
+        'publicKey' => env('BTREE_PUBLIC_KEY'),
+        'privateKey' => env('BTREE_PRIVATE_KEY')
+    ]);
+
+    $amount = $request->amount;
+    $nonce = $request->payment_method_nonce;
+
+    $result = $gateway->transaction()->sale([
+        'amount' => $amount,
+        'paymentMethodNonce' => $nonce,
+        'customer' => [
+            'firstName' => 'Tony',
+            'lastName' => 'Stark',
+            'email' => 'tony@avengers.com',
+        ],
+        'options' => [
+            'submitForSettlement' => true
+        ]
+    ]);
+
+
+    if ($result->success) {
+        $transaction = $result->transaction;
+        // header("Location: transaction.php?id=" . $transaction->id);
+
+        return back()->with('success_message', 'Transaction successful. The ID is:'. $transaction->id);
+    } else {
+        $errorString = "";
+
+        foreach ($result->errors->deepAll() as $error) {
+            $errorString .= 'Error: ' . $error->code . ": " . $error->message . "\n";
+        }
+
+        // $_SESSION["errors"] = $errorString;
+        // header("Location: index.php");
+        return back()->withErrors('An error occurred with the message: '.$result->message);
+    }
+});
